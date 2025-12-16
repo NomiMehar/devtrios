dd5f7ed5d9d1480b8b41d799373c124f37f979d5
Usama <151726675+Legendsusama@users.noreply.github.com>
2025-12-16 18:20:53 +0500

    app feature

---
+++ app/(components)/app-featured-case/AppFeaturedCase.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./AppFeaturedCase.module.scss"
+
+
+
+export default function AppFeaturedCase(){
+    return (
+        <section>
+            <h2>Hello world</h2>
+        </section>
+    )
+}
